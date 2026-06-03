fa1b68e9123714ed4ab636745b0d7279824099f0
Juan Velasco <jvelasco@uazuay.edu.ec>
2026-06-03 00:00:03 -0500

    feat: Product evolucionado a entidad JPA (@Entity, @Id, @GeneratedValue)

--- src/main/java/com/cedia/smartinventory/model/Product.java
+++ src/main/java/com/cedia/smartinventory/model/Product.java
@@ -1,5 +1,6 @@
 package com.cedia.smartinventory.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,13 +10,17 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String description;
     private Double price;
     private Integer stock;
     private Boolean active;
-
 }
